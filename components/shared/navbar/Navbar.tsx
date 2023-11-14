cc89d7f8896158d4d8b3b02a2c8c857b479acd69
Verica Timova <vera.timova@outlook.com>
2023-11-14 23:08:15 +0100

    Mobile nav implemented and finished nav ui

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import React from 'react'
 import Theme from "./Theme";
 import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
@@ -19,6 +20,7 @@ const Navbar = () => {
                 Dev <span className='text-primary-500'>Overflow</span>
             </p>
         </Link>
+        <GlobalSearch />
         <div className='flex-between gap-5'>
             <Theme />
             <SignedIn>
@@ -34,7 +36,6 @@ const Navbar = () => {
                     }}
                 />
             </SignedIn>
-
         <MobileNav />
         </div>
     </nav>
