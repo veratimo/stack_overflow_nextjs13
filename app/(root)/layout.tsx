0abdf4f511d4c43cbb7a0f51dd4176b69be875da
Verica Timova <vera.timova@outlook.com>
2023-11-15 22:27:43 +0100

    Left side bar set up

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSidebar from '@/components/shared/LeftSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const Layout = ({ children }: {children: React.ReactNode }) => {
   return (
     <main className='background-light850_dark100 relative'>
         <Navbar />
+        <LeftSidebar /> 
         <div className='flex'>
 
             <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14'>
